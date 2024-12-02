6fdac50bd19888c2640768046978b811f7bd4b25
Evan Wise <evan.d.wise@gmail.com>
2024-12-01 19:07:42 -0600

    feat(main): solve day 1

---
+++ day_01/src/main.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+use std::error::Error;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut list1 = vec![0; 1000];
+    let mut list2 = vec![0; 1000];
+    if let Ok(lines) = read_lines("./data/lists.txt") {
+        for (i, line) in lines.flatten().enumerate() {
+            let mut iter = line.split("   ");
+            let str1 = iter.next().ok_or("Malformed line")?;
+            let num1 = str1.parse::<i32>()?;
+            let str2 = iter.next().ok_or("Malformed line")?;
+            let num2 = str2.parse::<i32>()?;
+            list1[i] = num1;
+            list2[i] = num2;
+        }
+    }
+    list1.sort();
+    list2.sort();
+
+    let mut sum = 0;
+    for (l, r) in list1.iter().zip(list2.iter()) {
+        sum += (l - r).abs();
+    }
+    println!("The sum is: {}", sum);
+
+    let mut cum_score = 0;
+    let mut score_by_id = HashMap::new();
+    for location_id in list1.iter() {
+        if let Some(score) = score_by_id.get(&location_id) {
+            cum_score += score;
+        } else {
+            let mut count = 0;
+            for other_id in list2.iter() {
+                if other_id == location_id {
+                    count += 1;
+                }
+            }
+            let score = location_id * count;
+            score_by_id.insert(location_id, score);
+            cum_score += score;
+        }
+    }
+    println!("The similarity score is: {}", cum_score);
+
+
+    Ok(())
+}
