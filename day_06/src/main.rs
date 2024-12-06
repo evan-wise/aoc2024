8bae19d6e212f1258c36b7be22bd5fb896d818dd
Evan Wise <evan.d.wise@gmail.com>
2024-12-06 17:51:28 -0600

    chore(main): clean up a bit

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -28,6 +28,7 @@ impl Iterator for FileCharIterator {
     }
 }
 
+#[derive(Clone, Debug)]
 enum Direction {
     Up,
     Down,
@@ -35,33 +36,74 @@ enum Direction {
     Right,
 }
 
+#[derive(Debug)]
 enum CellState {
     Unvisited,
     Visited,
     Obstacle,
 }
 
-struct State {
+#[derive(Clone, Debug)]
+struct Guard {
     pub position: (usize, usize),
     pub direction: Direction,
-    pub map: Vec<Vec<CellState>>,
 }
 
-impl State {
-    pub fn new() -> State {
-        State {
+impl Guard {
+    pub fn new() -> Guard {
+        Guard {
             position: (0, 0),
             direction: Direction::Up,
-            map: Vec::new(),
         }
     }
 }
 
+struct State {
+    map: Vec<Vec<CellState>>,
+    guard: Guard,
+    history: Vec<Guard>,
+}
+
+impl State {
+    pub fn new(map: Vec<Vec<CellState>>) -> State {
+        State {
+            map: map,
+            guard: Guard::new(),
+            history: Vec::new(),
+        }
+    }
+
+    pub fn map(&self) -> &Vec<Vec<CellState>> {
+        &self.map
+    }
+
+    pub fn guard(&self) -> &Guard {
+        &self.guard
+    }
+
+    pub fn history(&self) -> &Vec<Guard> {
+        &self.history
+    }
+
+    pub fn move_guard(&mut self, position: (usize, usize)) -> &Guard {
+        self.guard.position = position;
+        self.map[position.0][position.1] = CellState::Visited;
+        self.history.push(self.guard.clone());
+        &self.guard
+    }
+
+    pub fn turn_guard(&mut self, direction: Direction) -> &Guard {
+        self.guard.direction = direction;
+        &self.guard
+    }
+}
+
 fn parse_input(chars: FileCharIterator) -> Result<State, Box<dyn Error>> {
-    let mut state = State::new();
+    let mut map = Vec::new();
     let mut row = Vec::new();
     let mut i = 0;
     let mut j = 0;
+    let mut position = (0, 0);
     for char in chars.flatten() {
         match char {
             '.' => {
@@ -74,11 +116,11 @@ fn parse_input(chars: FileCharIterator) -> Result<State, Box<dyn Error>> {
             },
             '^' => {
                 row.push(CellState::Visited);
-                state.position = (i, j);
+                position = (i, j);
                 j += 1;
             },
             '\n' => {
-                state.map.push(row);
+                map.push(row);
                 row = Vec::new();
                 i += 1;
                 j = 0;
@@ -88,11 +130,13 @@ fn parse_input(chars: FileCharIterator) -> Result<State, Box<dyn Error>> {
             },
         }
     }
+    let mut state = State::new(map);
+    state.move_guard(position);
     Ok(state)
 }
 
 fn check_bounds(state: &State) -> bool {
-    let (i, j) = state.position;
+    let (i, j) = state.guard.position;
     let height = state.map.len();
     if height == 0 {
         return false;
@@ -101,7 +145,7 @@ fn check_bounds(state: &State) -> bool {
     if width == 0 {
         return false;
     }
-    match state.direction {
+    match state.guard.direction {
         Direction::Up => i != 0,
         Direction::Down => i < height - 1,
         Direction::Left => j != 0,
@@ -114,23 +158,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut state = parse_input(chars)?;
     let mut num_positions = 1;
     loop {
-        let (i, j) = state.position;
-        match state.direction {
+        let (i, j) = state.guard().position;
+        match state.guard().direction {
             Direction::Up => {
                 if !check_bounds(&state) {
                     break;
                 }
-                match state.map[i-1][j] {
+                match state.map()[i-1][j] {
                     CellState::Obstacle => {
-                        state.direction = Direction::Right;
+                        state.turn_guard(Direction::Right);
                     },
                     CellState::Unvisited => {
-                        state.map[i-1][j] = CellState::Visited;
-                        state.position = (i-1, j);
+                        state.move_guard((i-1, j));
                         num_positions += 1;
                     },
                     CellState::Visited => {
-                        state.position = (i-1, j);
+                        state.move_guard((i-1, j));
                     },
                 }
             },
@@ -138,17 +181,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                 if !check_bounds(&state) {
                     break;
                 }
-                match state.map[i+1][j] {
+                match state.map()[i+1][j] {
                     CellState::Obstacle => {
-                        state.direction = Direction::Left;
+                        state.turn_guard(Direction::Left);
                     },
                     CellState::Unvisited => {
-                        state.map[i+1][j] = CellState::Visited;
-                        state.position = (i+1, j);
+                        state.move_guard((i+1, j));
                         num_positions += 1;
                     },
                     CellState::Visited => {
-                        state.position = (i+1, j);
+                        state.move_guard((i+1, j));
                     },
                 }
             },
@@ -156,17 +198,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                 if !check_bounds(&state) {
                     break;
                 }
-                match state.map[i][j-1] {
+                match state.map()[i][j-1] {
                     CellState::Obstacle => {
-                        state.direction = Direction::Up;
+                        state.turn_guard(Direction::Up);
                     },
                     CellState::Unvisited => {
-                        state.map[i][j-1] = CellState::Visited;
-                        state.position = (i, j-1);
+                        state.move_guard((i, j-1));
                         num_positions += 1;
                     },
                     CellState::Visited => {
-                        state.position = (i, j-1);
+                        state.move_guard((i, j-1));
                     },
                 }
             },
@@ -174,22 +215,22 @@ fn main() -> Result<(), Box<dyn Error>> {
                 if !check_bounds(&state) {
                     break;
                 }
-                match state.map[i][j+1] {
+                match state.map()[i][j+1] {
                     CellState::Obstacle => {
-                        state.direction = Direction::Down;
+                        state.turn_guard(Direction::Down);
                     },
                     CellState::Unvisited => {
-                        state.map[i][j+1] = CellState::Visited;
-                        state.position = (i, j+1);
+                        state.move_guard((i, j+1));
                         num_positions += 1;
                     },
                     CellState::Visited => {
-                        state.position = (i, j+1);
+                        state.move_guard((i, j+1));
                     },
                 }
             },
         }
     }
     println!("The guard visits {} position(s) on the map.", num_positions);
+    println!("History length: {}", state.history().len());
     Ok(())
 }
