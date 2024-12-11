b011d699486330da41278a766a671c2495a8930b
Evan Wise <evan.d.wise@gmail.com>
2024-12-11 12:45:09 -0600

    feat(main): optimize algo and solve day 11 part 2

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,4 +1,5 @@
 use crate::days::Solution;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::num::ParseIntError;
 
@@ -7,40 +8,59 @@ pub struct Day11;
 impl Solution for Day11 {
     fn solve(&self) -> Result<(), Box<dyn std::error::Error>> {
         let raw = read_to_string("./data/day11.txt")?;
-        let mut stones: Vec<u64> = raw.split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect();
-        for _ in 0..25 {
-            blink(&mut stones)?;
+        let stones: Vec<u64> = raw.split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+        let mut stone_count_by_num = HashMap::new();
+        for &stone in &stones {
+            if let Some(&count) = stone_count_by_num.get(&stone) {
+                stone_count_by_num.insert(stone, count + 1);
+            } else {
+                stone_count_by_num.insert(stone, 1 as u64);
+            }
         }
-        println!("There are {} stone(s) after 25 blinks", stones.len());
+        for _ in 0..25 {
+            stone_count_by_num = blink(&stone_count_by_num)?;
+        }
+        println!("There are {} stone(s) after 25 blinks", get_total(&stone_count_by_num));
+        for _ in 0..50 {
+            stone_count_by_num = blink(&stone_count_by_num)?;
+        }
+        println!("There are {} stone(s) after 75 blinks", get_total(&stone_count_by_num));
         Ok(())
     }
 }
 
-fn blink(stones: &mut Vec<u64>) -> Result<(), ParseIntError> {
-    let mut num_stones = stones.len();
-    let mut i = 0;
-    while i < num_stones {
-        match stones[i] {
-            0 => {
-                stones[i] = 1;
-                i += 1;
-            },
+fn get_total(stone_count_by_num: &HashMap<u64, u64>) -> u64 {
+    let mut total = 0;
+    for (_, &count) in stone_count_by_num {
+        total += count;
+    }
+    total
+}
+
+fn blink(stone_count_by_num: &HashMap<u64, u64>) -> Result<HashMap<u64, u64>, ParseIntError> {
+    let mut new = HashMap::new();
+    for (&stone, &stone_count) in stone_count_by_num {
+        let new_stones = match stone {
+            0 => vec![1],
             _ => {
-                let stone_str = stones[i].to_string();
+                let stone_str = stone.to_string();
                 let stone_len = stone_str.len();
                 if stone_len % 2 == 0 {
-                    let half = stone_len / 2;
-                    let first = stone_str[0..half].parse::<u64>()?;
-                    let second = stone_str[half..stone_len].parse::<u64>()?;
-                    stones.splice(i..=i, [first, second]);
-                    i += 2;
-                    num_stones += 1;
+                    let half = (stone_len / 2) as u32;
+                    let divisor = (10 as u64).pow(half);
+                    vec![stone / divisor, stone % divisor]
                 } else {
-                    stones[i] = 2024 * stones[i];
-                    i += 1;
+                    vec![2024 * stone]
                 }
+            },
+        };
+        for &new_stone in &new_stones {
+            if let Some(&count) = new.get(&new_stone) {
+                new.insert(new_stone, stone_count + count);
+            } else {
+                new.insert(new_stone, stone_count);
             }
         }
     }
-    Ok(())
+    Ok(new)
 }
