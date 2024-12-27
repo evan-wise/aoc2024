6e828a86fa94af1bc4cefaac1533a44582881fa2
Evan Wise <evan.d.wise@gmail.com>
2024-12-26 20:27:20 -0600

    feat(main): solve day 16 part 2

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -2,6 +2,7 @@ use crate::aoc::{read_lines, Direction, Position, Solution, SolutionParts};
 use std::cmp::Reverse;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::error::Error;
+use std::fmt::Display;
 use std::path::Path;
 
 pub struct Day16;
@@ -9,7 +10,7 @@ pub struct Day16;
 impl Solution for Day16 {
     fn solve(&self) -> Result<SolutionParts, Box<dyn Error>> {
         let map = parse_input("./data/day16.txt")?;
-        let (score, num_seats) = minimal_path(&map);
+        let (score, num_seats) = minimal_paths(&map);
         Ok((Some(score.to_string()), Some(num_seats.to_string())))
     }
 }
@@ -39,48 +40,89 @@ fn parse_input<P: AsRef<Path>>(path: P) -> Result<Map, Box<dyn Error>> {
     Ok(Map::new(grid, start, end))
 }
 
-fn minimal_path(map: &Map) -> (usize, usize) {
+fn minimal_paths(map: &Map) -> (usize, usize) {
     let mut visited = HashSet::new();
-    let mut binary_heap = BinaryHeap::from([(Reverse(0 as usize), map.start, Direction::Right)]);
-    let mut scores = Vec::new();
-    let mut predecessors_by_pos: HashMap<Position, HashSet<Position>> = HashMap::new();
-    while let Some((Reverse(score), pos, dir)) = binary_heap.pop() {
+    let mut binary_heap =
+        BinaryHeap::from([(Reverse(0 as usize), map.start, Direction::Right, None)]);
+    let mut low_scores = HashMap::new();
+    let mut low_scoring_prevs: HashMap<(Position, Direction), HashSet<(Position, Direction)>> =
+        HashMap::new();
+    let mut low_score = usize::MAX;
+    let mut end_dirs = Vec::new();
+    while let Some((Reverse(score), pos, dir, maybe_prev)) = binary_heap.pop() {
         if pos == map.end {
-            scores.push(score);
+            if score <= low_score {
+                low_score = score;
+                end_dirs.push(dir);
+            } else {
+                continue;
+            }
         }
 
-        if visited.insert((pos, dir)) {
-            for (d, s) in [
-                (dir, score + 1),
-                (dir.right(), score + 1001),
-                (dir.left(), score + 1001),
-            ] {
-                if let Some((x, y)) = map.next_pos(pos, d) {
-                    if let Cell::Empty = map.grid[y][x] {
-                        binary_heap.push((Reverse(s), (x, y), d));
-                        predecessors_by_pos
-                            .entry((x, y))
-                            .or_insert_with(HashSet::new)
-                            .insert(pos);
-                    }
-                }
+        let prev_score = *low_scores.get(&(pos, dir)).unwrap_or(&usize::MAX);
+        if score <= prev_score {
+            low_scores.insert((pos, dir), score);
+            if let Some(prev) = maybe_prev {
+                low_scoring_prevs
+                    .entry((pos, dir))
+                    .or_insert_with(HashSet::new)
+                    .insert(prev);
+            }
+        }
+
+        if !visited.insert((pos, dir)) {
+            continue;
+        }
+
+        for (d, s) in [
+            (dir, score + 1),
+            (dir.right(), score + 1001),
+            (dir.left(), score + 1001),
+        ] {
+            if let Some(((x, y), Cell::Empty)) = map.next(pos, d) {
+                binary_heap.push((Reverse(s), (x, y), d, Some((pos, dir))));
             }
         }
     }
 
-    // This is wrong, I am counting ALL the paths not just the minimal ones...
-    // Not sure how to modify this at the moment, gonna come back to it.
     let mut nodes = HashSet::new();
-    let mut stack = vec![map.end];
-    while let Some(pos) = stack.pop() {
-        if nodes.insert(pos) {
+    let mut visited = HashSet::new();
+    let mut stack = end_dirs
+        .iter()
+        .map(|d| (map.end, *d))
+        .collect::<Vec<(Position, Direction)>>();
+    while let Some((pos, dir)) = stack.pop() {
+        nodes.insert(pos);
+        if visited.insert((pos, dir)) {
             let empty = HashSet::new();
-            let predecessors = predecessors_by_pos.get(&pos).unwrap_or(&empty);
-            stack.extend(predecessors);
+            let prevs = low_scoring_prevs.get(&(pos, dir)).unwrap_or(&empty);
+            stack.extend(prevs);
         }
     }
 
-    (*scores.iter().min().unwrap_or(&0), nodes.len())
+    (low_score, nodes.len())
+}
+
+// For debugging
+#[allow(dead_code)]
+fn print_map(map: &Map, nodes: &HashSet<Position>) {
+    for (y, row) in map.grid.iter().enumerate() {
+        println!(
+            "{}",
+            row.iter()
+                .enumerate()
+                .map(|(x, c)| if (x, y) == map.end {
+                    "E".to_string()
+                } else if (x, y) == map.start {
+                    "S".to_string()
+                } else if nodes.contains(&(x, y)) {
+                    "O".to_string()
+                } else {
+                    format!("{c}")
+                })
+                .collect::<String>()
+        );
+    }
 }
 
 #[derive(Debug)]
@@ -108,33 +150,33 @@ impl Map {
         }
     }
 
-    fn next_pos(&self, pos: Position, dir: Direction) -> Option<Position> {
+    fn next(&self, pos: Position, dir: Direction) -> Option<(Position, Cell)> {
         let (x, y) = pos;
         match dir {
             Direction::Up => {
                 if y >= 1 {
-                    Some((x, y - 1))
+                    Some(((x, y - 1), self.grid[y - 1][x]))
                 } else {
                     None
                 }
             }
             Direction::Down => {
                 if y + 1 < self.height {
-                    Some((x, y + 1))
+                    Some(((x, y + 1), self.grid[y + 1][x]))
                 } else {
                     None
                 }
             }
             Direction::Left => {
                 if x >= 1 {
-                    Some((x - 1, y))
+                    Some(((x - 1, y), self.grid[y][x - 1]))
                 } else {
                     None
                 }
             }
             Direction::Right => {
                 if x + 1 < self.width {
-                    Some((x + 1, y))
+                    Some(((x + 1, y), self.grid[y][x + 1]))
                 } else {
                     None
                 }
@@ -143,7 +185,7 @@ impl Map {
     }
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 enum Cell {
     Empty,
     Wall,
@@ -155,6 +197,14 @@ impl Cell {
             '#' => Ok(Cell::Wall),
             '.' | 'S' | 'E' => Ok(Cell::Empty),
             _ => Err("invalid character".to_string()),
+        }
+    }
+}
+impl Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Cell::Wall => write!(f, "#"),
+            Cell::Empty => write!(f, "."),
         }
     }
 }
