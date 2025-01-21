91340d6bac8a01b6b83f51a3212eb6d243053cc1
Evan Wise <evan.d.wise@gmail.com>
2025-01-21 03:38:10 -0600

    feat(main): solve day 22 part 1

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,21 +1,46 @@
-use crate::aoc::{Answers, Solution};
+use crate::aoc::{read_lines, Answers, Solution};
 use std::error::Error;
 
 #[derive(Debug)]
-pub struct Day22;
+pub struct Day22 {
+    seeds: Vec<usize>,
+}
 
 impl Day22 {
     pub fn new() -> Day22 {
-        Day22 {}
+        Day22 { seeds: Vec::new() }
     }
 }
 
 impl Solution for Day22 {
     fn parse_input(&mut self) -> Result<(), Box<dyn Error>> {
+        let filename = "./data/day22.txt";
+        let lines = read_lines(filename)?;
+        self.seeds = lines.flatten().map(|l| l.parse::<usize>()).collect::<Result<Vec<_>, _>>()?;
         Ok(())
     }
 
     fn solve(&mut self) -> Result<Answers, Box<dyn Error>> {
-        Ok(Answers::None)
+        let part1: usize = self.seeds.iter().map(|s| {
+            let mut result = *s;
+            for _ in 0..2000 {
+                result = prng(result);
+            }
+            result
+        }).sum();
+        Ok(Answers::part1(part1))
     }
+}
+
+const CUTOFF: usize = 2usize.pow(24);
+
+fn prng(val: usize) -> usize {
+    let mut result = val;
+    result ^= result * 64;
+    result %= CUTOFF;
+    result ^= result / 32;
+    result %= CUTOFF;
+    result ^= result * 2048;
+    result %= CUTOFF;
+    result
 }
