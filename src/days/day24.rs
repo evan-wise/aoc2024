1e7467314f6cd31e128e84ee2a73adc700bf2e47
Evan Wise <evan.d.wise@gmail.com>
2025-02-04 18:46:39 -0600

    feat(main): solve day 24 part 2

--- src/days/day24.rs
+++ src/days/day24.rs
@@ -48,38 +48,114 @@ impl Solution for Day24 {
             let left = parts.next().ok_or("missing left operand")?.to_string();
             let operator = parts.next().ok_or("missing operator")?;
             let right = parts.next().ok_or("missing right operand")?.to_string();
-            let gate = match operator {
-                "AND" => Ok(Gate::And(left, right, output)),
-                "OR" => Ok(Gate::Or(left, right, output)),
-                "XOR" => Ok(Gate::Xor(left, right, output)),
+            self.gates.push_back(match operator {
+                "AND" => Ok(Gate {
+                    left,
+                    right,
+                    output,
+                    op: Op::And,
+                }),
+                "OR" => Ok(Gate {
+                    left,
+                    right,
+                    output,
+                    op: Op::Or,
+                }),
+                "XOR" => Ok(Gate {
+                    left,
+                    right,
+                    output,
+                    op: Op::Xor,
+                }),
                 _ => Err("invalid operator"),
-            }?;
-            self.gates.push_back(gate);
+            }?);
         }
         Ok(())
     }
 
     fn solve(&mut self) -> Result<Answers, Box<dyn Error>> {
-        while let Some(gate) = self.gates.pop_front() {
-            if !gate.eval(&mut self.wires) {
-                self.gates.push_back(gate);
+        let sorted_gates = self.simulate();
+        let part1 = self.read('z');
+        // We are assuming that no gates have been switched to "equivalent" positions, e.g.
+        // swapping two carry outputs. This is in principle detectable by tracking which input gate
+        // we started at but at least in my input there were no such swaps.
+        let mut bad_gates: Vec<Gate> = Vec::new();
+        for gate in &sorted_gates {
+            let mut parents = sorted_gates
+                .iter()
+                .filter(|g| g.output == gate.left || g.output == gate.right)
+                .map(|g| g.op.clone())
+                .collect::<Vec<Op>>();
+            parents.sort();
+            let mut children = sorted_gates
+                .iter()
+                .filter(|g| g.left == gate.output || g.right == gate.output)
+                .map(|g| g.op.clone())
+                .collect::<Vec<Op>>();
+            children.sort();
+            match (
+                gate.op.clone(),
+                &parents[..],
+                &children[..],
+            ) {
+                // input gates
+                (Op::And, [], [Op::Or]) |
+                (Op::Xor, [], [Op::And, Op::Xor]) |
+                // output gates
+                (Op::Xor, [Op::Or, Op::Xor], []) |
+                // carry gates
+                (Op::And, [Op::Or, Op::Xor], [Op::Or]) |
+                (Op::Or, [Op::And, Op::And], [Op::And, Op::Xor]) |
+                // special case for first carry gate
+                (Op::And, [Op::And, Op::Xor], [Op::Or]) => (),
+                // special cases for first input gates
+                (Op::And, [], [Op::And, Op::Xor]) |
+                (Op::Xor, [], []) if gate.left == "x00" || gate.right == "x00" => (),
+                // special case for second output gate
+                (Op::Xor, [Op::And, Op::Xor], []) if gate.output == "z01" => (),
+                // special case for last output gate
+                (Op::Or, [Op::And, Op::And], []) if gate.output == "z45" => (),
+                // anything else is wrong
+                _ => {
+                    // if we already found the parent gate ignore the child
+                    if let Some(_) = bad_gates.iter().find(|g| g.output == gate.left || g.output == gate.right) {
+                        continue;
+                    }
+                    bad_gates.push(gate.clone());
+                },
             }
         }
-        let part1 = self.read_output();
-        Ok(Answers::part1(part1))
+        let mut bad_outputs: Vec<String> =
+            bad_gates.into_iter().map(|g| g.output.clone()).collect();
+        bad_outputs.sort();
+        let part2 = bad_outputs.join(",");
+        Ok(Answers::both(part1, part2))
     }
 }
 
 impl Day24 {
-    fn read_output(&self) -> usize {
-        let mut z_wires: Vec<String> = self
+    fn simulate(&mut self) -> Vec<Gate> {
+        let mut sorted_gates = Vec::new();
+        let mut gates = self.gates.clone();
+        while let Some(gate) = gates.pop_front() {
+            if !gate.eval(&mut self.wires) {
+                gates.push_back(gate);
+            } else {
+                sorted_gates.push(gate)
+            }
+        }
+        sorted_gates
+    }
+
+    fn read(&self, c: char) -> usize {
+        let mut wires: Vec<String> = self
             .wires
             .keys()
-            .filter(|k| k.starts_with("z"))
+            .filter(|k| k.chars().nth(0).unwrap() == c)
             .cloned()
             .collect();
-        z_wires.sort();
-        z_wires.iter().enumerate().fold(0, |a, (i, wire)| {
+        wires.sort();
+        wires.iter().enumerate().fold(0, |a, (i, wire)| {
             a + if *self.wires.get(wire).unwrap_or(&false) {
                 2usize.pow(i as u32)
             } else {
@@ -89,35 +165,43 @@ impl Day24 {
     }
 }
 
-#[derive(Debug)]
-enum Gate {
-    And(String, String, String),
-    Or(String, String, String),
-    Xor(String, String, String),
+#[derive(Hash, PartialOrd, Ord, PartialEq, Eq, Clone, Debug)]
+enum Op {
+    And,
+    Or,
+    Xor,
+}
+
+#[derive(Hash, PartialEq, Eq, Clone, Debug)]
+struct Gate {
+    left: String,
+    right: String,
+    output: String,
+    op: Op,
 }
 
 impl Gate {
     fn eval(&self, wires: &mut FxHashMap<String, bool>) -> bool {
-        match self {
-            Self::And(left, right, output) => {
-                if let (Some(&l), Some(&r)) = (wires.get(left), wires.get(right)) {
-                    wires.insert(output.clone(), l & r);
+        match self.op {
+            Op::And => {
+                if let (Some(&l), Some(&r)) = (wires.get(&self.left), wires.get(&self.right)) {
+                    wires.insert(self.output.clone(), l & r);
                     true
                 } else {
                     false
                 }
             }
-            Self::Or(left, right, output) => {
-                if let (Some(&l), Some(&r)) = (wires.get(left), wires.get(right)) {
-                    wires.insert(output.clone(), l | r);
+            Op::Or => {
+                if let (Some(&l), Some(&r)) = (wires.get(&self.left), wires.get(&self.right)) {
+                    wires.insert(self.output.clone(), l | r);
                     true
                 } else {
                     false
                 }
             }
-            Self::Xor(left, right, output) => {
-                if let (Some(&l), Some(&r)) = (wires.get(left), wires.get(right)) {
-                    wires.insert(output.clone(), l ^ r);
+            Op::Xor => {
+                if let (Some(&l), Some(&r)) = (wires.get(&self.left), wires.get(&self.right)) {
+                    wires.insert(self.output.clone(), l ^ r);
                     true
                 } else {
                     false
